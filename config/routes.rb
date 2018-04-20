2a3bb3091ba1bf3601d36e5d721b69e2dee64d82
Fraz Ahsan <fraz.ahsan17@gmail.com>
2018-04-20 23:57:15 +0500

    pps added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/contact' => 'welcome#contact'
   get '/portfolio' => 'welcome#portfolio'
+  get '/pps' => 'welcome#pps'
   root 'welcome#index'
 end
