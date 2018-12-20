09fb2b9a6549655ee883faa979a147e90317681d
Fraz Ahsan <fraz.ahsan17@gmail.com>
2018-12-20 21:00:43 +0500

    pss apps disabled

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/contact' => 'welcome#contact'
   get '/portfolio' => 'welcome#portfolio'
-  get '/manifest' => 'welcome#manifest'
-  get '/pps' => 'welcome#pps'
-  get '/ipa' => 'welcome#ipa'
-  get '/test' => 'welcome#test'
-  get '/testpps' => 'welcome#testpps'
-  get '/testipa' => 'welcome#testipa'
+  # get '/manifest' => 'welcome#manifest'
+  # get '/pps' => 'welcome#pps'
+  # get '/ipa' => 'welcome#ipa'
+  # get '/test' => 'welcome#test'
+  # get '/testpps' => 'welcome#testpps'
+  # get '/testipa' => 'welcome#testipa'
   root 'welcome#index'
 end
